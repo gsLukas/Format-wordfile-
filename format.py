052ec465c8a9c8ed87a1b6d1a135875d17afa933
Grupo Sales Tecnologias <91630131+gsLukas@users.noreply.github.com>
2023-04-20 18:59:22 -0300

    Modificaç~ao completa do codigo

--- format.py
+++ format.py
@@ -1,43 +1,117 @@
-#!/usr/bin python
+import os
+import shutil
+import click
 
-from colors import BColors
-from wordsFile import WordsFile
-from time import sleep
-from os import system, path
 
-def format_words_file(input_file_path, output_file_path, min_length, max_length):
-    """Reads a file of words, formats them and writes to another file."""
-    if not path.exists(input_file_path):
-        print(f"{BColors.FAIL}File does not exist!{BColors.RESET}")
-        return
+@gruposales.command()
+def opcao1():
+    click.echo('Opção 1 selecionada')
+
+@gruposales.command()
+def opcao2():
+    click.echo('Opção 2 selecionada')
+
+if __name__ == '__main__':
+    click.clear()
+    click.echo('Bem-vindo(a) ao Grupo Sales!')
+    gruposales()  
+
+@click.group()
+def cli():
+    pass
+
+def formatar(arquivo, tamanho):
+    with open(arquivo, 'r') as f:
+        linhas = f.readlines()
     
-    print(f'{BColors.FAIL}\nFormatting please wait for writing confirmation!{BColors.RESET}')
-    with open(output_file_path, 'w') as output_file:
-        for word in WordsFile(input_file_path):
-            word = "".join(w for w in word.split())
-            if len(word) >= min_length and len(word) <= max_length:
-                output_file.write(word + "\n")
+    total_linhas = len(linhas)
+    metade = total_linhas // 2
+    
+    palavras = set()
+    
+    for i, linha in enumerate(linhas):
+        if i == metade:
+            click.echo('50% do processo concluído')
+        
+        linha = linha.strip()
+        if linha.isalpha() and len(linha) >= tamanho:
+            palavras.add(linha.lower())
+    
+    palavras = sorted(palavras)
+    
+    with open('Word list atualizada', 'w') as f:
+        for palavra in palavras:
+            f.write(palavra + '\n')
+    
+    click.echo('Arquivo formatado com sucesso!')
 
-system("clear")
-sleep(2)
-system('clear')
-print(f"Current Directory: [{path.abspath(path.curdir)}]")
 
-input_file_path = input(f"{BColors.OK}Input file path: {BColors.RESET}")
-output_file_path = input(f"{BColors.OK}Output file path: {BColors.RESET}")
+def corrigir(arquivo):
+    # Função que corrige a lista de palavras
+    if not os.access(arquivo, os.R_OK):
+        click.echo(f'Não foi possível ler o arquivo {arquivo}')
+        return
 
-option = input(f"{BColors.OK}Select an option:\n1 - Words between 8 and 64 characters\n2 - Words between 1 and 256 characters\n3 - Words between 8 and 128 characters\n4 - Words between 1 and 14 characters\n5 - Words between 8 and 128 characters\n{BColors.RESET}")
+    with open(arquivo, 'r') as f:
+        linhas = f.readlines()
 
-if not option.isdigit() or int(option) not in range(1, 6):
-    print(f"{BColors.FAIL}Invalid option!{BColors.RESET}")
-else:
-    if int(option) == 1:
-        format_words_file(input_file_path, output_file_path, 8, 64)
-    elif int(option) == 2:
-        format_words_file(input_file_path, output_file_path, 1, 256)
-    elif int(option) == 3:
-        format_words_file(input_file_path, output_file_path, 8, 128)
-    elif int(option) == 4:
-        format_words_file(input_file_path, output_file_path, 1, 14)
-    else:
-        format_words_file(input_file_path, output_file_path, 8, 128)
+    total_linhas = len(linhas)
+    metade = total_linhas // 2
+
+    novas_linhas = []
+
+    for i, linha in enumerate(linhas):
+        if i == metade:
+            click.echo('50% do processo concluído')
+
+        linha = linha.strip()
+
+        # Verifica se a palavra tem caracteres sequenciais iguais
+        if len(set(linha)) < 2:
+            continue
+
+        # Verifica se a linha tem mais de duas combinações de caracteres iguais
+        if any(linha[j] == linha[j+1] for j in range(len(linha)-1) for k in range(j+1, len(linha)-1) if linha[k] == linha[k+1]):
+            continue
+
+        novas_linhas.append(linha)
+
+    with open('Word list atualizada corrigida', 'w') as f:
+        for linha in novas_linhas:
+            f.write(linha + '\n')
+
+    click.echo('Arquivo corrigido com sucesso!')
+
+@cli.command()
+@click.argument('arquivo')
+@click.option('--tamanho', default=3, help='Tamanho mínimo das palavras')
+def formatar_wordlist(arquivo, tamanho):
+    if not os.access(arquivo, os.R_OK):
+        click.echo(f'Não foi possível ler o arquivo {arquivo}')
+        return
+
+    try:
+        formatar(arquivo, tamanho)
+    except Exception as e:
+        click.echo(f'Ocorreu um erro ao formatar o arquivo: {str(e)}')
+        return
+
+    click.echo('Arquivo formatado com sucesso!')
+
+@cli.command()
+@click.argument('arquivo')
+def corrigir_wordlist(arquivo):
+    if not os.access(arquivo, os.R_OK):
+        click.echo(f'Não foi possível ler o arquivo {arquivo}')
+        return
+
+    try:
+        corrigir(arquivo)
+    except Exception as e:
+        click.echo(f'Ocorreu um erro ao corrigir o arquivo: {str(e)}')
+        return
+
+    click.echo
+
+
+  
